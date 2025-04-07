f1129e38626df2c41abf7fb73d1e9c50b7c7ef0e
Hridesh MG <hridesh699@gmail.com>
2025-04-07 23:18:36 +0530

    add pdf vectorisation

--- test.py
+++ test.py
@@ -2,8 +2,14 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
+from langchain_core.prompts import ChatPromptTemplate
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite", temperature=0.7)
+system_template = "Translate the following from English into {language}"
 
-response = llm.invoke("What's the capital of France?")
-print(response.content)
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
+
+prompt.to_messages()
