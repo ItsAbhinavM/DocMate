bbe33f037e3eab2d22fcd52bd2b62d911b7ded2e
amansxcalibur <notifyamanvs@gmail.com>
2025-04-07 23:54:22 +0530

    chore: removed face recognition models dependecy

--- orchestrator/models/llm.py
+++ orchestrator/models/llm.py
@@ -11,7 +11,7 @@ if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = os.environ['GOOGLE_API_KEY']
 
 gemini_pro = ChatGoogleGenerativeAI(
-    model="gemini-pro",
+    model="gemini-2.0-flash",
     safety_settings={
         HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
     },
