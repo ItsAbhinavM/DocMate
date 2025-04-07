c9fe7a5a77297fa3adb531f09890c122e36f7261
amansxcalibur <notifyamanvs@gmail.com>
2025-04-07 21:39:23 +0530

    frontend init

---
+++ llm-agent/index.js
@@ -0,0 +1,98 @@
+const { app, BrowserWindow, session } = require('electron/main')
+const path=require('path')
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    transparent: true,
+    vibrancy:{
+      theme: 'light',
+      effect: 'acrylic',
+      disableOnBlur: true
+    },
+    //frame: false,
+    webPreferences:{
+      enableRemoteModule: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, "./preload.js"),
+  }
+	// webPreferences:{
+	// 	preload: 'preload.js'
+	// }
+	
+  })
+
+  win.loadFile('index.html')
+
+  session.defaultSession.setPermissionRequestHandler((webContents, permission, callback) => {
+    if (permission === 'media') {
+      callback(true); // Grant media permissions
+    } else {
+      callback(false);
+    }
+  });
+  
+
+
+
+}
+
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+    	createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+"use strict"
+
+//require("./App");
+
+const { ipcMain } = require("electron");
+
+// const getMyFriends = () => {
+// 	return [
+// 		{id: 6030, name: "Twilley"},
+// 		{id: 967, name: "Wickkiser"},
+// 		{id: 5073, name: "Essick"},
+// 		{id: 8886, name: "Marotta"},
+// 		{id: 7416, name: "Banh"}
+// 	];	
+// }
+
+// const sendMessage = (message) => {
+// 	console.log(message);
+// }
+
+// ipcMain
+// 	.on("main-window-ready", (e) => {
+// 		console.log("Main window is ready");
+// 	})
+// 	.on("send-message", (e, message) => {
+// 		sendMessage(message);
+// 	})
+
+// ipcMain.handle("get-friends", (e) => {
+// 	return getMyFriends();
+// })
+
+ipcMain.on('activate-app', ()=>{
+	console.log('Voice activation succesfull');
+	if(win){
+		win.show();
+	}
+})
---
+++ llm-agent/init.js
@@ -0,0 +1,82 @@
+document.getElementById("chat-form").addEventListener("submit", (e) => {
+	e.preventDefault();
+	window.api.sendMessage();
+})
+// document.getElementById("get-friends").addEventListener("click", async (e) => {
+// 	const friends = await window.api.getFriends();
+// 	const friendList = document.getElementById("friend-list");
+// 	friendList.innerHTML = "";
+// 	friendList.append(...friends.map(x => {
+// 		const li = document.createElement("li");
+// 		li.innerText = `[${x.id}] ${x.name}`;
+// 		return li;
+// 	}));
+// })
+document.getElementById("nav-btn").addEventListener("click", async (e) => {
+	let w=document.getElementById("nav").style.display;
+	console.log(w);
+	if (w!="none" || w==""){
+		document.getElementById("nav").style.display="none";
+	}else{
+		document.getElementById("nav").style.display="flex";
+	}
+})
+
+let history={};
+
+document.getElementById("send-chat").addEventListener("click", async (e) => {
+	e.preventDefault();
+	const prompt=window.api.sendFormData();
+	console.log(prompt);
+	if (prompt!=undefined){
+	ResponseText(prompt)
+	}
+})	
+
+function ResponseText(prompt){
+	window.api.getData(prompt)
+}
+document.addEventListener('DOMContentLoaded', function () {
+	const chatBox = document.getElementById('chat-box');
+	chatBox.addEventListener('input', function () {
+		autoExpand(chatBox);
+	});
+});
+
+function autoExpand(textarea) {
+	textarea.style.height = 'auto';
+	textarea.style.height = textarea.scrollHeight + 'px';
+}
+
+document.getElementById("chat-box").addEventListener("focus", async (e) => {
+	document.getElementById("dispose").style.flex='0';
+	document.getElementById('chat-form').style.maxWidth="100%";
+	document.getElementById('chat-box').placeholder="Type your prompt here..."
+})
+
+document.getElementById("chat-box").addEventListener("focusout", async (e) => {
+	setTimeout(()=>{
+		console.log(document.getElementById('content').innerHTML);
+		if (document.getElementById('content').innerHTML==""){
+			document.getElementById("dispose").style.flex='1';
+			document.getElementById('chat-form').style.maxWidth="40vw";
+			window.api.checkFormData();
+	}},200)
+})
+// document.addEventListener('DOMContentLoaded', () => {
+//     const form = document.getElementById('chat-form');
+//     const inputField = document.getElementById('chat-box');
+
+//     inputField.addEventListener('keydown', (event) => {
+//         if (event.key === 'Enter') {
+//             event.preventDefault();  // Prevent the default action (form submission) on Enter key press
+//             form.submit();           // Manually submit the form
+//         }
+//     });
+// });
+// document.getElementById('chat-form').addEventListener('submit', async(e)=>{
+// 	setTimeout(()=>{
+// 		e.preventDefault();
+// 		window.api.sendFormData();
+// 	})
+// })
