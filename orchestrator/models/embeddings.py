18b95e3cd333e689e32f880faf92f7b6e13bcc1f
Hridesh MG <hridesh699@gmail.com>
2025-04-09 05:23:24 +0530

    chore: remove unused files

--- orchestrator/models/embeddings.py
+++
@@ -1,4 +0,0 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
-all_MiniLM_L6_v2 = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-paraphrase_multilingual_mpnet_base_v2 = HuggingFaceEmbeddings(model_name="paraphrase-multilingual-mpnet-base-v2")
